cacf61f64da090584d6f8d5639fa71b90fb7745f
柯基 <keji@mogu.com>
2020-10-14 14:44:20 +0800

    代码提交

---
+++ src/cn/caoler/base/TreeNode.java
@@ -0,0 +1,13 @@
+package cn.caoler.base;
+
+/**
+ * @author keji
+ * @desc
+ * @date 2020/10/14 11:28 上午
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
