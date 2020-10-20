c3b812a31e1e6233a467c5b77c83501876dc3838
柯基 <caoler.cn@gmail.com>
2020-10-20 13:44:33 +0800

    第35题

---
+++ src/cn/caoler/list/ImplementStr.java
@@ -0,0 +1,23 @@
+package cn.caoler.list;
+
+/**
+ * @author keji
+ * @desc https://leetcode-cn.com/problems/implement-strstr/
+ * @date 2020/10/19 2:35 下午
+ */
+public class ImplementStr {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) {
+            return 0;
+        }
+        return haystack.indexOf(needle);
+    }
+
+    public static void main(String[] args) {
+        String hay = "aaahhbj";
+        String nel = "hbj";
+        ImplementStr implementStr = new ImplementStr();
+        int i = implementStr.strStr(hay, nel);
+        System.out.println(i);
+    }
+}
---
+++ src/cn/caoler/list/SearchInsertPosition.java
@@ -0,0 +1,25 @@
+package cn.caoler.list;
+
+/**
+ * @author keji
+ * @desc https://leetcode-cn.com/problems/search-insert-position/
+ * @date 2020/10/20 1:40 下午
+ */
+public class SearchInsertPosition {
+
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,5,6};
+        SearchInsertPosition searchInsertPosition = new SearchInsertPosition();
+        int i = searchInsertPosition.searchInsert(nums, 7);
+        System.out.println(i);
+    }
+}
