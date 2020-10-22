54d6a3f4fa6ac0ea5daf23217788a76100d008ee
柯基 <caoler.cn@gmail.com>
2020-10-22 14:05:14 +0800

    189

--- src/cn/caoler/list/RotateArray.java
+++ src/cn/caoler/list/RotateArray.java
@@ -37,10 +37,43 @@ public class RotateArray {
         }
     }
 
+    /**
+     * 用反转法
+     *这个方法基于这个事实：当我们旋转数组 k 次， k%n 个尾部元素会被移动到头部，剩下的元素会被向后移动。
+     *
+     * 在这个方法中，我们首先将所有元素反转。然后反转前 k 个元素，再反转后面 n−k 个元素，就能得到想要的结果。
+     *
+     *假设 n=7 且 k=3 。
+     *
+     * 原始数组                  : 1 2 3 4 5 6 7
+     * 反转所有数字后             : 7 6 5 4 3 2 1
+     * 反转前 k 个数字后          : 5 6 7 4 3 2 1
+     * 反转后 n-k 个数字后        : 5 6 7 1 2 3 4 --> 结果
+     *
+     */
+    public void rotate1(int[] nums, int k) {
+        int length = nums.length;
+        reveres(nums,0,length - 1);
+
+        int s = k%length;
+        reveres(nums,0,s -1);
+        reveres(nums,s,length -1);
+    }
+
+    public void reveres(int nums[], int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start ++;
+            end --;
+        }
+    }
+
     public static void main(String[] args) {
-        int[] nums = new int[]{1,2};
+        int[] nums = new int[]{1,2,3,4,5,6,7};
         RotateArray rotateArray = new RotateArray();
-        rotateArray.rotate(nums,3);
+        rotateArray.rotate1(nums,3);
         for (int num : nums) {
             System.out.print(num + ",");
         }
