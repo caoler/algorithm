736c15080eecb06c13656b8d2f82084a813a7310
柯基 <caoler.cn@gmail.com>
2020-11-03 14:09:47 +0800

    第69题

--- src/cn/caoler/easy/Sqrtx.java
+++ src/cn/caoler/easy/Sqrtx.java
@@ -6,5 +6,24 @@ package cn.caoler.easy;
  * @date 2020/11/3 1:53 下午
  */
 public class Sqrtx {
+    public int mySqrt(int x) {
+        int result = -1, l = 0, r = x;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if ((long)mid * mid <= x) {
+                result = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return result;
+    }
 
+    public static void main(String[] args) {
+        Sqrtx sqrtx = new Sqrtx();
+        int i = sqrtx.mySqrt(8);
+        System.out.println(i);
+
+    }
 }
