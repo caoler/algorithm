08c02ca35d99fa18b37af9381df25cf32d404d0f
柯基 <caoler.cn@gmail.com>
2020-11-18 16:44:52 +0800

    剑指offer3

---
+++ src/cn/caoler/offer/Offer5.java
@@ -0,0 +1,12 @@
+package cn.caoler.offer;
+
+/**
+ * @author keji
+ * @desc https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
+ * @date 2020/11/18 4:43 下午
+ */
+public class Offer5 {
+    public String replaceSpace(String s) {
+        return s.replaceAll(" ","%20");
+    }
+}
